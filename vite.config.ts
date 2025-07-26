6530bee26978f0c6c7ec568d0465cc879a732a2b
Arban Hossain <arbanhossain@gmail.com>
2025-07-26 18:12:31 +0600

    automatically build to gh-pages branch

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import path from 'path';
+import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/cf-elo-trainer/',
+      plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
